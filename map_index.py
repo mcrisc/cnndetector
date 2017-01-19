bc0da15be515b7b16e644b09e819b300fb2ce7ea
Marcelo Criscuolo <criscuolo.marcelo@gmail.com>
2017-01-19 16:14:05 -0200

    tokenizing and mapping

---
+++ map_index.py
@@ -0,0 +1,39 @@
+"""Map words to integer indices.
+"""
+import argparse
+
+SENTENCE_SIZE = 50
+
+
+def build_vocab_index(fpath):
+    with open(fpath) as fin:
+        index = {line.strip(): i for i, line in enumerate(fin)}
+    return index
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Map words to integer indices')
+    parser.add_argument('vocabulary', help='vocabulary file')
+    parser.add_argument('tokenized', help='tokenized dataset file')
+    parser.add_argument('outfile', help='basename of output file')
+    args = parser.parse_args()
+
+    # building vocabulary
+    vocab_index = build_vocab_index(args.vocabulary)
+    pad = vocab_index['<PAD>']
+
+    # mapping words to indices
+    with open(args.tokenized) as fin, open(args.outfile, 'w') as fout:
+        for line in fin:
+            sentence, label = line.strip().split('\t')
+            tokens = sentence.split()[:SENTENCE_SIZE]  # truncating
+            indices = [vocab_index[token] for token in tokens
+                       if token in vocab_index]
+            indices.extend([pad] * (SENTENCE_SIZE - len(indices)))
+            print(' '.join(map(str, indices)), label, sep='\t', file=fout)
+    print('finished')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tokenizer.py
@@ -0,0 +1,60 @@
+"""Tokenize a dataset and exports the vocabulary.
+"""
+import argparse
+import re
+from pathlib import Path
+
+RE_TOKENS = re.compile(r'\b\S+\b')
+RE_DIGITS = re.compile(r'\d')
+
+
+def load_data(fname):
+    with open(fname) as fin:
+        contents = [tuple(line.strip().split('\t')) for line in fin]
+    return contents
+
+
+def tokenize(data):
+    """Tokenize sentences.
+    """
+    tokenized = [(RE_TOKENS.findall(RE_DIGITS.sub('0', sent)), label)
+                 for sent, label in data]
+    return tokenized
+
+
+def compile_vocabulary(tokenized_data):
+    vocab = set()
+    for sent, _ in tokenized_data:
+        vocab.update(sent)
+    vocab = ['<PAD>'] + sorted(vocab)
+    return vocab
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Tokenize dataset')
+    parser.add_argument('data', help='dataset file')
+    parser.add_argument('outfile', help='basename of output file')
+    args = parser.parse_args()
+
+    print('tokenizing:', args.data)
+    data = load_data(args.data)
+    tokenized = tokenize(data)
+    vocab = compile_vocabulary(tokenized)
+
+    # writting vocabulary file
+    vocab_file = Path(args.outfile).with_suffix('.voc')
+    with vocab_file.open('w') as fout:
+        for word in vocab:
+            print(word, file=fout)
+
+    # writting tokenized file
+    tokenized_file = Path(args.outfile).with_suffix('.tok')
+    with tokenized_file.open('w') as fout:
+        for tokens, label in tokenized:
+            print(' '.join(tokens), label, sep='\t', file=fout)
+
+    print('finished')
+
+
+if __name__ == '__main__':
+    main()
